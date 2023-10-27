e4a73ef9e3c7af1807d8e609070be0753817b39a
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-28 01:33:26 +0300

    3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 
 morgan.token('json', (req, res) => req.method === "POST" ? JSON.stringify(req.body) : null)
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 
