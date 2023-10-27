0e826b016c35071527b38da73a8aa9e1c493ca84
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-27 22:17:17 +0300

    3.3

--- index.js
+++ index.js
@@ -2,30 +2,39 @@ const express = require('express')
 const app = express()
 
 let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(data)
+    res.json(data)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const person = data.find(person => person.id === Number(req.params.id))
+    if (person)
+        res.json(person)
+    else
+        res.status(404).end()
+
 })
 
 app.get('/info', (req, res) => {
