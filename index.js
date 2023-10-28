c225a0db79f8052f6387d6572ead4fb27d2175c9
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-28 17:23:02 +0300

    3.15

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const Person = require('./models/person')
 morgan.token('json', (req, res) => req.method === "POST" ? JSON.stringify(req.body) : null)
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
-app.use(express.static('dist'))
 
 
 app.get('/api/persons', (req, res) => {
@@ -19,13 +19,25 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
     Person.findById(req.params.id)
-        .then(person => res.json(person))
-        .catch(() => res.status(404).end())
+        .then(person => {
+            if (person) res.json(person)
+            else res.status(404).end()
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).send({ error: "malformed id" })
+        })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    data = data.filter(person => person.id !== Number(req.params.id))
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).end()
+        })
 })
 
 app.get('/info', (req, res) => {
@@ -34,13 +46,21 @@ app.get('/info', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
     if (!req.body.name || !req.body.number)
-        return res.status(400).json({error: "incomplete entry", request: req.body})
+        return res.status(400).json({ error: "incomplete entry", request: req.body })
     const person = new Person({
         name: req.body.name,
         number: req.body.number
     })
     person.save().then(result => res.json(person))
 })
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, "0.0.0.0", () => console.log(`Server up on port ${PORT}`))
