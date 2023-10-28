6968b164d346986c987bb121015240cffb53dc91
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-28 18:25:53 +0300

    3.20

--- models/person.js
+++ models/person.js
@@ -9,7 +9,15 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
       },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: v => /^\d{2}\d?-\d+$/.test(v),
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
 })
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
